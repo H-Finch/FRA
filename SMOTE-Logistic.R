4ec13a0745ddc037920507d7dddb5f25e94bb963
H-Finch <vijay.somanath@outlook.com>
2016-06-17 06:47:46 +0530

    Update SMOTE-Logistic.R

--- SMOTE-Logistic.R
+++ SMOTE-Logistic.R
@@ -22,27 +22,16 @@
 # Arun Venkatesan
 # Rajagopalan Kannan
 # Vijay Somanath
-#
+# Unbalanced (ubOver) with logistic
 #_________________________________________________
 
-
-
-
-#_________________________________________________
-#
 #  Clean up
-#
-#_________________________________________________
 rm(list=ls())
 gc()
 
-#_________________________________________________
-#
 # Load packages
-#
-#_________________________________________________
 require(caret)
-#require(plyr)
+require(plyr)
 #require(Hmisc)
 require(pROC)
 require(DMwR)
@@ -54,35 +43,21 @@ require(DMwR)
 require(Amelia)
 require(ROCR)
 require(unbalanced)
-library(plyr)
 
-#_________________________________________________
-#
+
 # Read files
-#
-#_________________________________________________
 #setwd('c:/r/data/csv/')
 train <- read.csv('fra_train.csv', header=TRUE)
 test <- read.csv('fra_test.csv', header=TRUE)
 
 
-#_________________________________________________
-#
 # Study the data set
-#
-#_________________________________________________
 dim(train)
 dim(test)
 str(train)
 str(test)
 
-#_________________________________________________
-#
 # Check missing values
-#
-#_________________________________________________
-
-# visuals
 missmap(train, col=c('red', 'white'), legend=FALSE, y.cex=0.3,
         main='training data set - missingness map')
 missmap(test, col=c('red', 'white'), legend=FALSE, y.cex=0.3,
@@ -102,61 +77,25 @@ prop.table(table(test$SeriousDlqin2yrs))
 pie(table(train$SeriousDlqin2yrs))
 
 
-#_________________________________________________
-#
-# Let's do some exploratory
-#
-#_________________________________________________
-# source('helper.R')
-# hgraph(train[,-1])
-# hgraph(test[,-1])
-
-# Casenum                 Identifier
-# SeriousDlqin2yrs        Categorical - outcome variable
-# RevolvingUtilizationOfUnsecuredLines  Continuous (Skewed)
-# DebtRatio               Continuous (Skewed)
-# NumberOfOpenCreditLinesAndLoans Continuous (Skewed)
-# NumberOfDependents      Categorical
-
 ggplot(train, aes(NumberOfDependents,fill=factor(SeriousDlqin2yrs))) +
   geom_bar(position="dodge")+coord_flip()
 
-#_________________________________________________
-#
+
 # Check for unbalanced classes
-#
-#_________________________________________________
-
-
 
 # Debt ratio & Revolving utilization of unsecured loans
-
 ggplot(train,aes(x=factor(train$SeriousDlqin2yrs),y=DebtRatio))+geom_boxplot()
-
 ggplot(train, aes(x=factor(train$SeriousDlqin2yrs),y=train$RevolvingUtilizationOfUnsecuredLines))+geom_boxplot()
 
-
-#_________________________________________________
-#
 # Data Pre-Processing
-#
-#_________________________________________________
 
-# predictors must be numeric for roc function
-train$SeriousDlqin2yrs <- factor(train$SeriousDlqin2yrs)
+train$SeriousDlqin2yrs <- factor(train$SeriousDlqin2yrs)# predictors must be numeric for roc function
 test$SeriousDlqin2yrs <- factor(test$SeriousDlqin2yrs)
 
 #train$NumberOfDependents <- factor(train$NumberOfDependents)
 #test$NumberOfDependents <- factor(test$NumberOfDependents)
 
-
-
-#_________________________________________________
-#
 # Mode Imputation
-#
-#_________________________________________________
-
 
 getmode <- function(v) {
   uniqv <- unique(v)
@@ -177,52 +116,14 @@ ggplot(train1, aes(x=factor(train1$SeriousDlqin2yrs),y=train1$RevolvingUtilizati
 #outlier treatment - Debt ratio
 train2=subset(train1,(train1$DebtRatio<=100))
 str(train2)
-p=ggplot(train2,aes(x=factor(train2$SeriousDlqin2yrs),y=train2$DebtRatio))+geom_boxplot()
-(p)
-#_________________________________________________
-#
-# Now check missing values
-#
-#_________________________________________________
+ggplot(train2,aes(x=factor(train2$SeriousDlqin2yrs),y=train2$DebtRatio))+geom_boxplot()
 
+# Now check missing values
 sapply(train, function(x) sum(is.na(x)))
 sapply(test, function(x) sum(is.na(x)))
 
 
-
-#_________________________________________________
-#
-# Derived variables
-#
-#_________________________________________________
-
-
-
-
-#_________________________________________________
-#
-# Log Transformation
-#
-#_________________________________________________
-
-
-
-#_________________________________________________
-#
-# Bin - Transformation
-#
-#_________________________________________________
-
-
-
-
-
-
-#_________________________________________________
-#
 # Create Dummy Variables
-#
-#_________________________________________________
 
 # dummies <- dummyVars(~ NumberOfDependents,
 #                      data=train)
@@ -235,12 +136,7 @@ sapply(test, function(x) sum(is.na(x)))
 # dummy_var <- as.data.frame(predict(dummies, test))
 # test <- cbind(test[,c(1,2,3,4,5)],dummy_var)
 
-#_________________________________________________
-#
 # Split Data
-#
-#_________________________________________________
-
 # Remove S.No attribute
 df <- train2
 df <- df[,-1]
@@ -253,28 +149,18 @@ df_train <- df[trainIndex,];df_valid <- df[-trainIndex,]
 # Training scheme - 10-fold CV
 ctrl <- trainControl(method='repeatedcv', number=10,repeats=3)
 
-#_________________________________________________
-#
 # Declare outcome and features
-#
-#_________________________________________________
 outcome <- 'SeriousDlqin2yrs'
 features <- setdiff(names(df_train), outcome)
 
-
-
-#_________________________________________________
-#
-# let's SMOTE on training data set, not on valid, test data sets
-#
-#_________________________________________________
+# ubOver on training data set, not on valid, test data sets
 require(unbalanced)
 
 # 6% of data set is of class 1 and this is minority class.
 prop.table(table(df_train$SeriousDlqin2yrs))
 prop.table(table(df_valid$SeriousDlqin2yrs))
 
-#Before SMOTE
+#Before ubOver
 prop.table(table(train2$SeriousDlqin2yrs))
 
 ggplot(train, aes(NumberOfOpenCreditLinesAndLoans, 
@@ -301,13 +187,8 @@ df_train=newdf_train
 prop.table(table(df_train$SeriousDlqin2yrs))
 dim(df_train)
 
-#_________________________________________________
-#
-# train the SMOTE data
-#
-#_________________________________________________
 
-
+# train the oversampled data
 fit <- glm(SeriousDlqin2yrs~.,
            data=df_train,
            family = 'binomial')
